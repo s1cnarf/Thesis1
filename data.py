b3d5c0a7a3cce6b2d53108bd4047715041814796
s1cnarf <201910986@fit.edu.ph>
2022-10-24 00:16:10 +0800

    Modify Notes method

--- data.py
+++ data.py
@@ -25,6 +25,11 @@ def Notes(pattern, text):
 
     size = len(pattern['event'])
 
+    notes_in_truth = []
+    notes_in_user = []
+
+    truth = []
+
     correct, partial, extra, missed = 0, 0, 0, 0
     # The p
     # notes = [i for i in range(0,len(pattern['event'])) if pattern['event'][i] == 'NOTE_ON']
@@ -33,30 +38,81 @@ def Notes(pattern, text):
         # correct hit
     # Else if p time not equal t time
         # test 
-    for i in range(0, size):
-        if pattern['event'][i] == 'Note_on':
-            if pattern['start'][i] == text['start'][i]: # 0 0
-                correct += 1
+    start, end = 0, 0
+    for i in range (0, len(text['event'])):
+        if text['end'][i] > end :
+            end = text['end'][i]
+            start = text['start'][i] 
+            # line 14 1,17234,19000,Note_on,1,30,0
+            #print (f'start: {start} end: {end}')
+            for p in range(0,size):
+                if pattern['start'][p] >= start and pattern['end'][p] <= end:
+                    notes_in_user.append([pattern['start'][p], # 0 , 1 , 2
+                                        pattern['end'][p],
+                                        pattern['note'][p]])
+
+            for t in range(0,len(text['event'])):
+                if text['start'][t] >= start and text['end'][t] <= end:
+                    notes_in_truth.append([text['start'][t], # 0 , 1 , 2
+                                        text['end'][t],
+                                        text['note'][t]])
+
+            
+            #print(f'truth: {notes_in_truth}')
+            #print(f'user: {notes_in_user}')
+            
+            truth = [0] * len(notes_in_truth)
+            user = [0] * len(notes_in_user)
+
+            #print(f'truth: {truth}')
+            #print(f'user: {user}')
+            for idx, i in enumerate(notes_in_truth): 
+                print (i)
                 
-            elif pattern['start'][i] < text['start'][i]: # early
-                partial += 1
-            elif pattern['start'][i] > text['start'][i]: # late
-                partial += 1
+                if truth[idx] == 0:
+                    for y in range(0, len(notes_in_user)):
+                        #Time -> Notes:
+                        # 0 = Start 1 = End 2 = Note
+                        # Problem if tama yung time frame and napindot ulet like
+                        # double pindot with true note
+                        if user[y] == 0:
+                            if notes_in_user[y][0] == i[0] and notes_in_user[y][1] == i[1]: #start end
+                                if notes_in_user[y][2] == i[2]: #note number
+                                    #print(f'Correct: {notes_in_user[y][2]} = {i[2]}, {y}')
+                                    truth[idx] = 1
+                                    user[y] = 1
+                                    correct += 1
+                                # elif notes_in_user[y][2] != i[2]:
+                                #     #print(f'Extra: {notes_in_user[y][2]} = {i[2]}, {y}')
+                                #     truth[idx] = 3
+                                #     user[y] = 3
+                                #     extra += 1
+                            elif notes_in_user[y][0] >= i[0] and notes_in_user[y][1] <= i[1]:
+                                if notes_in_user[y][2] == i[2]:
+                                    #print(f'Partial: {notes_in_user[y][2]} = {i[2]}, {y}')
+                                    partial += 1
+                                    truth[idx] = 2
+                                    user[y] = 2  
+                                # elif notes_in_user[y][2] != i[2]:
+                                #     #print(f'Extra: {notes_in_user[y][2]} = {i[2]}, {y}')
+                                #     truth[idx] = 3 
+                                #     user[y] = 3 
+                                #     extra += 1
+                                    
+            #print (f'1 TABLE TRUTH: {truth}')
+            #truth = Missed(truth, notes_in_truth, user, notes_in_user)
+           
 
-            else:
-                missed += 1
 
-            j = i
-            for j in range (i, size):
-                if pattern['start'][j] < pattern['end'][i]: # end - 1536
-                    if pattern['start'][j] == text['start'][j]: # 0 1536
-                        pass
-                    elif pattern['start'][j] != text['start'][j]:
-                        extra += 1
+            notes_in_truth.clear()
+            notes_in_user.clear()
+            
+            print (f'TABLE TRUTH: {truth}')
+            print (f'TABLE USER: {user}')
+            
 
+            print (correct, partial, extra, missed)
 
-
-    print (correct, partial, extra, missed)
 
 
 
